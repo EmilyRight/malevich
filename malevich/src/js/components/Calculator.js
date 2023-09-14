d45db5b5033a883370215d8a42496e263ae32fd1
PHOENIXIT\ekanarskaya <ekanarskaya@fil-it.ru>
2023-09-15 02:29:25 +0300

    init

---
+++ malevich/src/js/components/Calculator.js
@@ -0,0 +1,130 @@
+/* eslint-disable no-param-reassign */
+import { disabled } from 'express/lib/application';
+import devicesData from '../constants.js/devicesData';
+import { deviceLinkText, smartphoneLinkText } from '../constants.js/textContent';
+
+class Calculator {
+  constructor() {
+    this.labels = document.querySelectorAll('.input-line__label');
+    this.inputs = document.querySelectorAll('.input-line__input');
+    this.inputsBar = document.querySelector('.spots-line');
+    this.devices = document.querySelectorAll('.device');
+    this.basePriceHtml = document.querySelector('.price__old-price');
+    this.currentPriceHtml = document.querySelector('.price__new-price');
+    this.activeDeviceImage = document.querySelector('.calc-grid__image');
+    this.activeDeviceName = document.querySelector('.calc-grid__name');
+    this.activeDeviceLink = document.querySelector('.calc-grid__link');
+    this.activeDevice = devicesData[3];
+    this.activeItem = [...this.devices].find((device) => this.activeDevice.id === +device.id);
+    this.currentDiscount = this.activeDevice.maxDiscount;
+    console.log(this.activeDevice, this.currentDiscount);
+    this.basePrice = this.activeDevice.basePrice;
+    this.currentPrice = this.basePrice - this.currentDiscount;
+    this.handleInputbar();
+  }
+
+  removeActiveClass() {
+    this.devices.forEach((device) => {
+      if (device.classList.contains('active')) {
+        device.classList.remove('active');
+      }
+    });
+  }
+
+  removeDisabledInputs() {
+    this.inputs.forEach((input) => {
+      input.removeAttribute('disabled');
+      if (input.classList.contains('in-range')) {
+        input.classList.remove('in-range');
+      }
+    });
+  }
+
+  setActiveDevice(item) {
+    this.removeActiveClass();
+    this.removeDisabledInputs();
+    this.activeDevice = devicesData.find((device) => device.id === +item.id);
+    this.activeItem = item;
+    this.currentDiscount = this.activeDevice.maxDiscount;
+    item.classList.add('active');
+    this.handleInputbar();
+  }
+
+  showDeviceInfo() {
+    const {
+      name, imageSrc, basePrice, type,
+    } = this.activeDevice;
+    this.activeDeviceImage.src = imageSrc;
+    this.activeDeviceName.innerHTML = name;
+
+    this.activeDeviceLink.innerHTML = type === 'smartphone'
+      ? smartphoneLinkText
+      : deviceLinkText;
+    this.showDevicePrices();
+  }
+
+  showDevicePrices() {
+    const { basePrice } = this.activeDevice;
+    const basePriceArray = basePrice.toString().split('');
+    basePriceArray.splice(-3, 0, ' ');
+
+    const currentPriceArray = (basePrice - this.currentDiscount).toString().split('');
+    currentPriceArray.splice(-3, 0, ' ');
+
+    this.basePriceHtml.innerHTML = `${basePriceArray.join('')}&nbsp;₽`;
+    this.currentPriceHtml.innerHTML = `${currentPriceArray.join('')}&nbsp;₽`;
+  }
+
+  handleInputbar() {
+    const { discount } = this.activeItem.dataset;
+    this.removeDisabledInputs();
+    this.inputs.forEach((input, index) => {
+      if (input.id === discount
+        || input.id === this.currentDiscount
+        || Number(input.id) < Number(discount)) {
+        input.checked = true;
+        input.classList.add('in-range');
+        this.inputsBar.style.backgroundImage = `linear-gradient(to right, #3fcbff ${(100 / 6) * index}%, #d3d9df ${(100 / 6) * index}%)`;
+      }
+      if (Number(input.id) > Number(discount)) {
+        input.disabled = 'true';
+      }
+    });
+  }
+
+  handleDiscountChoice(event) {
+    const discount = event.target.htmlFor;
+    if (discount <= this.activeDevice.maxDiscount) {
+      this.currentDiscount = discount;
+      this.showDevicePrices();
+      this.inputs.forEach((input, index) => {
+        if (input.classList.contains('in-range')) {
+          input.classList.remove('in-range');
+        }
+        if (Number(input.id) <= Number(discount)) {
+          input.checked = true;
+          input.classList.add('in-range');
+          this.inputsBar.style.backgroundImage = `linear-gradient(to right, #3fcbff ${(100 / 6) * index + 1}%, #d3d9df ${(100 / 6) * index + 1}%)`;
+        }
+      });
+    }
+  }
+
+  addEventListeners() {
+    this.devices.forEach((device) => {
+      device.addEventListener('click', () => {
+        this.setActiveDevice(device);
+        this.showDeviceInfo();
+      });
+    });
+
+    this.labels.forEach((input) => {
+      input.addEventListener('click', (event) => {
+        this.handleDiscountChoice(event);
+        // this.handleInputbar();
+      });
+    });
+  }
+}
+
+export default Calculator;
---
+++ malevich/src/js/constants.js/devicesData.js
@@ -0,0 +1,52 @@
+const cameraImg = require('../../img/page/camera-left.png');
+const capsImg = require('../../img/page/caps-left.png');
+const winkImg = require('../../img/page/wink-left.png');
+const smartphone30Img = require('../../img/page/smartphone-left.png');
+const smartphone30iImg = require('../../img/page/smartphone30i-left.png');
+
+const devicesData = [
+  {
+    id: 1,
+    type: 'device',
+    name: 'Камера Ростелеком',
+    imageSrc: cameraImg,
+    maxDiscount: 1000,
+    basePrice: 5990,
+  },
+  {
+    id: 2,
+    type: 'device',
+    name: 'Капсула мини',
+    imageSrc: capsImg,
+    maxDiscount: 1000,
+    basePrice: 6990,
+  },
+
+  {
+    id: 3,
+    type: 'device',
+    name: 'TV-приставка Wink',
+    imageSrc: winkImg,
+    maxDiscount: 2000,
+    basePrice: 6990,
+  },
+
+  {
+    id: 4,
+    type: 'smartphone',
+    name: 'Infinix HOT 30 8/128Gb',
+    imageSrc: smartphone30Img,
+    maxDiscount: 4000,
+    basePrice: 17990,
+  },
+
+  {
+    id: 5,
+    type: 'smartphone',
+    name: 'Infinix NOTE 30i 8/256',
+    imageSrc: smartphone30iImg,
+    maxDiscount: 4000,
+    basePrice: 21990,
+  },
+];
+export default devicesData;
